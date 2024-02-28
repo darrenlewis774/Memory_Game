588447eb018354f39ba3fef2bbf568a2eb15287d
Darren Lewis <darrenlewis774@gmail.com>
2024-02-28 16:25:12 +0000

    Comment added to test merge

--- app.js
+++ app.js
@@ -85,6 +85,8 @@ function createBoard() {
   completed.style.display = "none"
 }
 
+// This comment I do not need!!
+
 function flipCard() {
   /* This function is invoked when a card created by function 'createBoard' is clicked on. The data id attribute for 
   this card is assigned to const variable cardId */
